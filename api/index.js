398926ef543e109bac6c89184f3f9adbee763f53
Nishat Yeasmin <nishatyasminnisha@gmail.com>
2026-05-21 22:16:09 +0600

    finally updated

--- api/index.js
+++ api/index.js
@@ -253,6 +253,8 @@ app.get('/', (req, res) => {
 // app.listen(port, () => {
 //     console.log(`Server running on port ${port}`);
 // });
+
+const app = require('../server')
 module.exports = app;
    
       
